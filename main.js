9cdb1f88bb302c9915fd613091c23bd3300b4c96
Lea Dalibard <leadalibard@gmail.com>
2020-12-15 16:55:37 +0100

    Learning : ng-repeat-start, ng-repeat-end

--- main.js
+++ main.js
@@ -2,14 +2,20 @@ angular.module("appNom", []) //[] pour les dépendances + bien mettre le nom dan
     .controller("mainCtrl", ["$scope", function ($scope) {
         $scope.name = "Sam";
         $scope.users=[
-            {name:"Sam"},
-            {name:"Julie"},
-            {name:"Jim"},
-            {name:"Anna"},
+            {name:"Sam", email:"sam@gmail.com"},
+            {name:"Julie",email:"julie@gmail.com"},
+            {name:"Jim",email:"jim@gmail.com"},
+            {name:"Anna",email:"anna@gmail.com"},
             ];
     }])//$scope-> variable modifiable dans controller
     .controller("childCtrl", ["$scope", function ($scope) {
     }])
     .controller("grantCtrl", ["$scope", function ($scope) {
         $scope.name = "Anna";
+    }])
+    .controller("userCtrl", ["$scope", function ($scope) {
+        $scope.user=
+            {name:"Sam",age:18,email:"sam@gmail.com"};
     }]);
+
+
