921ce3cde083af5659fef336e6d6eeb5e798609f
Lea Dalibard <leadalibard@gmail.com>
2020-12-16 12:33:07 +0100

    Making a function to orderby column

--- ex2/main.js
+++ ex2/main.js
@@ -1,6 +1,6 @@
 angular.module("appNomTwo", [])
     .controller("mainCtrl", ["$scope", function ($scope) {
-        $scope.teams=[
+        $scope.teams = [
             {name: "OL", pts: 54, j: 26, g: 16, n: 6},
             {name: "PSG", pts: 52, j: 26, g: 14, n: 10},
             {name: "OM", pts: 50, j: 26, g: 15, n: 5},
@@ -9,8 +9,24 @@ angular.module("appNomTwo", [])
             {name: "Bordeaux", pts: 41, j: 26, g: 11, n: 7},
             {name: "Montpellier", pts: 39, j: 25, g: 11, n: 8},
             {name: "Guinguamp", pts: 35, j: 26, g: 1, n: 13}
-            ];
+        ];
+        $scope.order = {
+            pts: "desc",
+            j: "desc",
+            g: "desc",
+            n: "desc"
+        };
+        $scope.sort = function (col) {
+            $scope.teams.sort(function (a, b) {
+                if ($scope.order[col] == "desc") {
+                    return a[col] - b[col];
+                } else {
+                    return a[col] + b[col];
+                }
+            });
+            $scope.order[col] = $scope.order[col] == "desc" ? "asc" : "desc";
+        }
     }])
-  ;
+
 
 
