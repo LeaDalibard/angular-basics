9c76170730b90ee4d1fe23c6ac493ea21e326d5d
Lea Dalibard <leadalibard@gmail.com>
2020-12-17 15:03:02 +0100

    Learned to make dynamic Tab component

---
+++ ex9bis/main.js
@@ -0,0 +1,31 @@
+angular.module("appNomTwo", [])
+    .controller("mainCtrl", ["$scope", function ($scope) {
+        $scope.tabs = [
+            {title: "Home", content: "Home"},
+            {title: "Profile", content: "Profile"},
+            {title: "Message", content: "Message"},
+            {title: "Settings", content: "Settings"},
+        ];
+
+        $scope.active = 0;
+
+        $scope.setActive = function (index) {
+            $scope.active = index;
+        }
+        $scope.isActive = function (index) {
+            return $scope.active == index;
+        }
+        $scope.removeTab = function (index) {
+            $scope.tabs.splice(index, 1);
+        }
+        $scope.addTab = function () {
+            $scope.tabs.push({
+                title: $scope.name,
+                content: $scope.content
+            });
+        }
+
+    }])
+
+
+
