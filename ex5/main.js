38a439969ce63772e9c41a4d526e3bc414d8589d
Lea Dalibard <leadalibard@gmail.com>
2020-12-17 10:32:59 +0100

    Learning checkbox, select options , ng-false-value, ng-true value

--- ex5/main.js
+++ ex5/main.js
@@ -1,9 +1,18 @@
-angular.module("appNomTwo", [])
-    .controller("mainCtrl", ["$scope", function ($scope) {
-      $scope.sampleCheckBox=true;
-      $scope.hasChanged=function(){
-          console.log("changé");
-      }
+angular.module('appNomTwo', [])
+    .controller('mainCtrl', ["$scope", function ($scope) {
+
+        $scope.sampleCheckBox = true
+
+        $scope.hasChanged = function () {
+            console.log("changé");
+        }
+        $scope.users=[
+            {name:"Sam", email:"sam@gmail.com"},
+            {name:"Julie", email:"juliesam@gmail.com"},
+            {name:"Jim", email:"jim@gmail.com"},
+            {name:"Anna", email:"anna@gmail.com"},
+        ];
+
     }])
 
 
