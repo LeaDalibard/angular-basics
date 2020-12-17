e4b5974bd9001e9748bd9a9cc3c1420d0bcd72de
Lea Dalibard <leadalibard@gmail.com>
2020-12-17 12:06:54 +0100

    Change CSS with ng-class, ng-style, ng-init

---
+++ ex8/main.js
@@ -0,0 +1,17 @@
+angular.module("appNomTwo", [])
+    .controller("mainCtrl", ["$scope", function ($scope) {
+        $scope.colors = [
+            "black",
+            "red",
+            "blue",
+            "green",
+            "purple",
+            "cyan",
+            "gold",
+            "yellow",
+            "lime"
+        ]
+    }])
+
+
+
